06a97dd30ad7a35b1a8b62d0deaeac63bd979eba
Krosumlabs <82745391+Krosumlabs@users.noreply.github.com>
2026-02-10 10:31:58 +0530

    Add files via upload

---
+++ DAY1/p1.py
@@ -0,0 +1,13 @@
+# simple python program
+# display my product detatils
+
+print("Product ID:101")
+print("Product Name:pA")
+print('Product pA Cost is:1234.55')
+
+'''
+using print function
+we can display
+our user defined message
+'''
+
---
+++ DAY1/p2.py
@@ -0,0 +1,10 @@
+# simple python program
+# display my product detatils
+
+pid = 102
+pname = 'pB'
+pcost = 3234.55
+
+print("Product ID:",pid)
+print("Product Name:",pname)
+print('Product',pname,'Cost is:',pcost)
