6e1ab7376c56f009ce7834e9c718d7d368f9286d
Krosumlabs <82745391+Krosumlabs@users.noreply.github.com>
2026-02-12 17:18:44 +0530

    Add files via upload

---
+++ DAY4/p4.py
@@ -0,0 +1,11 @@
+print("one")
+Fname = "emp.csv"
+try:
+    print("File name is:",fname)
+except Exception as eobj:
+    print(eobj)
+
+print("Test-1")
+print("Test-2")
+print("Test-3")
+print("End of the line")
